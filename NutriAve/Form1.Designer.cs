668b366811600f54e420a1adf33647db6f57548f
cwbmachado <cwbmachado@gmail.com>
2024-02-19 22:59:52 -0300

    Trabalho finalizado

--- NutriAve/Form1.Designer.cs
+++ NutriAve/Form1.Designer.cs
@@ -30,46 +30,99 @@ namespace NutriAve
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnAbrirPortaA = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAbrirPortaA
             // 
-            this.btnAbrirPortaA.Location = new System.Drawing.Point(53, 177);
+            this.btnAbrirPortaA.BackColor = System.Drawing.Color.Green;
+            this.btnAbrirPortaA.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrirPortaA.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAbrirPortaA.Location = new System.Drawing.Point(59, 579);
             this.btnAbrirPortaA.Name = "btnAbrirPortaA";
-            this.btnAbrirPortaA.Size = new System.Drawing.Size(137, 23);
+            this.btnAbrirPortaA.Size = new System.Drawing.Size(177, 61);
             this.btnAbrirPortaA.TabIndex = 0;
             this.btnAbrirPortaA.Text = "AbrirPortaA";
-            this.btnAbrirPortaA.UseVisualStyleBackColor = true;
+            this.btnAbrirPortaA.UseVisualStyleBackColor = false;
             this.btnAbrirPortaA.Click += new System.EventHandler(this.btnAbrirPortaA_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(635, 177);
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(948, 579);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.Size = new System.Drawing.Size(206, 61);
             this.button2.TabIndex = 1;
             this.button2.Text = "AbrirPortaB";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.btnAbrirPortaB_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(358, 218);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.Size = new System.Drawing.Size(68, 25);
             this.label1.TabIndex = 2;
-            this.label1.Text = "lblStatus";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "Status";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(497, 603);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Abrir Ambas Portas";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnAbrirAmbasPortas_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Crimson;
+            this.button3.Location = new System.Drawing.Point(559, 701);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "SAIR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(505, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NUTRIAVE 1.0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(977, 450);
+            this.BackColor = System.Drawing.Color.DarkOrchid;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1195, 790);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnAbrirPortaA);
@@ -88,6 +141,9 @@ namespace NutriAve
         private EventHandler button1_Click;
         private EventHandler button2_Click;
         private EventHandler btnAbrirPortaA_Click_1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- NutriAve/Form1.cs
+++ NutriAve/Form1.cs
@@ -5,44 +5,113 @@ namespace NutriAve
 {
     public partial class Form1 : Form
     {
-        public object lblStatus { get; private set; }
-
         public Form1()
         {
             InitializeComponent();
+
+            // Definindo o tamanho do formulário
+            this.Width = 1024; 
+            this.Height = 768; 
         }
+
 
         private void btnAbrirPortaA_Click(object sender, EventArgs e)
         {
-            label1.Text = "Porta A abrindo para alimentar as galinhas...";
-            var timer = new Timer { Interval = 10000 }; // 10 segundos
-            timer.Tick += (s, args) =>
-            {
-                label1.Text = "Porta A fechada.";
-                timer.Stop();
-            };
-            timer.Start();
+            AbrirPortaA();
         }
 
         private void btnAbrirPortaB_Click(object sender, EventArgs e)
         {
-            label1.Text = "Porta B abrindo para alimentar as galinhas...";
-            var timer = new Timer { Interval = 10000 }; // 10 segundos
-            timer.Tick += (s, args) =>
-            {
-                label1.Text = "Porta B fechada.";
-                timer.Stop();
-            };
-            timer.Start();
+            AbrirPortaB();
         }
 
-
-        private void button2_Click_1(object sender, EventArgs e)
+        private void btnAbrirAmbasPortas_Click(object sender, EventArgs e)
         {
+            int contador = 10; // Inicia o contador em 10 segundos
 
+            // Define o texto do label para mostrar que ambas as portas estão abrindo
+            label1.Text = $"Portas A e B abrindo para alimentar as galinhas... {contador} segundos restantes.";
+
+            // Configura um timer para contar de 1 em 1 segundo
+            var timerAmbas = new Timer { Interval = 1000 }; // 1 segundo
+
+            timerAmbas.Tick += (s, args) =>
+            {
+                contador--; // Decrementa o contador a cada tick
+                if (contador > 0)
+                {
+                    // Atualiza o texto do label para mostrar os segundos restantes
+                    label1.Text = $"Portas A e B abrindo para alimentar as galinhas... {contador} segundos restantes.";
+                }
+                else
+                {
+                    // Atualiza o texto do label após 10 segundos para indicar que ambas as portas estão fechadas
+                    label1.Text = "Portas A e B fechadas.";
+
+                    // Para e descarta o timer
+                    timerAmbas.Stop();
+                    timerAmbas.Dispose();
+                }
+            };
+
+            // Inicia o timer
+            timerAmbas.Start();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+
+        private void AbrirPortaA()
+        {
+            int contadorA = 10; // Inicia o contador em 10 segundos para a Porta A
+            label1.Text = $"Porta A abrindo para alimentar as galinhas... {contadorA} segundos restantes.";
+
+            var timerA = new Timer { Interval = 1000 }; // 1 segundo
+            timerA.Tick += (s, args) =>
+            {
+                contadorA--; // Decrementa o contador a cada tick
+                if (contadorA > 0)
+                {
+                    label1.Text = $"Porta A abrindo para alimentar as galinhas... {contadorA} segundos restantes.";
+                }
+                else
+                {
+                    label1.Text = "Porta A fechada.";
+                    timerA.Stop();
+                    timerA.Dispose();
+                }
+            };
+            timerA.Start();
+        }
+
+        private void AbrirPortaB()
+        {
+            int contadorB = 10; // Inicia o contador em 10 segundos para a Porta B
+            label1.Text = $"Porta B abrindo para alimentar as galinhas... {contadorB} segundos restantes.";
+
+            var timerB = new Timer { Interval = 1000 }; // 1 segundo
+            timerB.Tick += (s, args) =>
+            {
+                contadorB--; // Decrementa o contador a cada tick
+                if (contadorB > 0)
+                {
+                    label1.Text = $"Porta B abrindo para alimentar as galinhas... {contadorB} segundos restantes.";
+                }
+                else
+                {
+                    label1.Text = "Porta B fechada.";
+                    timerB.Stop();
+                    timerB.Dispose();
+                }
+            };
+            timerB.Start();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
